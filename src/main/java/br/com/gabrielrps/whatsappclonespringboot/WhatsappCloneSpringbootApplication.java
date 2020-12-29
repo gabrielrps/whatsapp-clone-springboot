eae7474445bd210f6d61a636054f0abefd1281e6
Gabriel Rezende <gabriel.rezendeps@gmail.com>
2020-12-29 16:27:25 -0300

    first commit

---
+++ src/main/java/br/com/gabrielrps/whatsappclonespringboot/WhatsappCloneSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielrps.whatsappclonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappCloneSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gabrielrps/whatsappclonespringboot/WhatsappCloneSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielrps.whatsappclonespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
